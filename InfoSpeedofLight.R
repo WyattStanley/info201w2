421c0cb0c8166c5cbbd7b64113e2e2437ef04a8d
Wyatt Stanley <wyattstanley@Wyatts-MacBook-Air.local>
2021-04-08 14:27:35 -0700

    add a script that computes lightyear

---
+++ InfoSpeedofLight.R
@@ -0,0 +1,8 @@
+c <- 29792458
+lightMinute <- c*60
+lightHour <- lightMinute*60
+lightDay <- lightHour*24
+lightYear <- lightDay*365.2422
+cat("length of light year:", lightYear, "\n")
+cat("thats too long")
+
